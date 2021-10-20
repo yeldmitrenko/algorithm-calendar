f061ecba10b2fe388f2bc429e1375db38d99a962
Yelizaveta Dmitrenko <74879089+yeldmitrenko@users.noreply.github.com>
2021-10-20 12:05:58 +0300

    Update testing.py

--- test/testing.py
+++ test/testing.py
@@ -1,31 +1,16 @@
-from models.insurance_type_list import InsuranceType
-from models.property_list import Property
-from models.benefits_list import Benefits
-from models.types_insure import CorporateInsurance, LifeInsurance, PropertyInsurance
-from models.corporate_list import CorporateInsurancesType
-from manager.insurance_mang import InsuranceManager
-from models.sort_order import SortOrder
-from models.insurance import Insurance
+import unittest
+from calendar import *
 
 
-def main():
-    manager1 = InsuranceManager()
-    VUSO_housing = PropertyInsurance(Property.HOUSE, 500000, 180000, InsuranceType.PROPERTY, 24, 10000, 15,
-                                     ['fire', 'robbery', 'tornado', 'burglary', 'flood'])
-    VUSO_people_help = LifeInsurance('Jack Jonson', 'John Snow', [Benefits.JURIDICAL_HELP], InsuranceType.LIFE, 12,
-                                     1500.5, 31,
-                                     ['deaths', 'trauma', 'fines'])
-    VUSO_company = CorporateInsurance('IT', 250, CorporateInsurancesType.CYBER_CASES, InsuranceType.CORPORATE, 72,
-                                      15000, 23, ['court', 'robbery', 'industrial accident'])
-    VS_insure = Insurance(InsuranceType.LIFE, 23, 14500, 23, ['deaths'])
+class MyTestCase(unittest.TestCase):
+    def test_sort(self):
+        self.assertEqual(sort_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]),
+                              [(0, 1), (3, 5), (4, 8), (9, 10), (10, 12)])
 
-    manager1.add_insurances(VS_insure)
-    manager1.add_insurances(VUSO_company)
-    manager1.add_insurances(VUSO_housing)
-    print(manager1.search_by_type(InsuranceType.LIFE))
-    print(manager1.sort_by_price(SortOrder.DESC))
-    print(manager1.sort_by_risk(SortOrder.ASC))
+    def test_merge(self):
+        self.assertEqual(merge_ranges([(0, 1), (3, 5), (4, 8), (9, 10), (10, 12)]),
+                         [(0, 1), (3, 8), (9, 12)])
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
